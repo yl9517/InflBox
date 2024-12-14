29a9e6a0299ca27319f21a02a6cf43ba302d72a0
이여름 <iyeoleum@iyeoleum-ui-MacBookAir.local>
2024-12-14 23:55:36 +0900

    delete: DB 설정 삭제

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,18 @@ import { CampainModule } from './campaign/campaign.module';
     ConfigModule.forRoot({
       isGlobal: true, // 전역적으로 환경 변수를 사용할 수 있도록 설정
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: process.env.DB_TYPE as 'mysql',
-        host: process.env.DB_HOST,
-        port: +process.env.DB_PORT,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // DB 자동 동기화 (개발 환경에서만 사용)
-      }),
-    }),
+    // TypeOrmModule.forRootAsync({
+    //   useFactory: () => ({
+    //     type: process.env.DB_TYPE as 'mysql',
+    //     host: process.env.DB_HOST,
+    //     port: +process.env.DB_PORT,
+    //     username: process.env.DB_USERNAME,
+    //     password: process.env.DB_PASSWORD,
+    //     database: process.env.DB_DATABASE,
+    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    //     synchronize: true, // DB 자동 동기화 (개발 환경에서만 사용)
+    //   }),
+    // }),
     CampainModule,
   ],
   controllers: [AppController],
